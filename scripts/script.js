ff2c4c85c24f5c532c3137fc759b6f8d974642e2
Ivan Semenov <ivsemenovdev@gmail.com>
2023-02-09 12:01:11 +0300

    feat: build a directory tree from a JSON file and add it to the page

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,44 @@ async function getData() {
     }
 }
 
-getData().then( data => {
-    console.log(data)
-})
+getData().then( data => renderTree(data) )
+
+const mainNode = document.getElementById('main-container');
+const menu = document.createElement('div');
+
+menu.setAttribute("id", "main-menu");
+mainNode.appendChild(menu);
+
+const node = document.getElementById('main-menu');
+
+function buildTree(children) {
+
+    let ul = document.createElement("ul");
+
+    for (let i = 0, n = children.length; i < n; i++) {
+
+        let branch = children[i];
+        let li = document.createElement("li");
+
+        branch.type === 'file' ?
+            li.innerHTML = `${branch.name}` :
+            li.innerHTML = `<div>${branch.name}`;
+
+        if (branch.children) {
+            li.appendChild(buildTree(branch.children));
+        } else if (branch.type === 'file') {
+            li.setAttribute('data-file', `${branch.path}`);
+        }
+
+        ul.appendChild(li);
+
+    }
+    return ul;
+}
+
+function renderTree(data) {
+    node.appendChild(buildTree(data));
+}
 
 
 
